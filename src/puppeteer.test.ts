2c7b907e5bb8a871a2e7134c141491120fe9afdf
Roman Filippov <rf@romanfilippov.com>
2020-04-14 19:11:29 +0700

    feat: adds puppeteer abstraction

---
+++ src/puppeteer.test.ts
@@ -0,0 +1,41 @@
+import { locateChromeExecutable, hasExtras, launch } from './puppeteer'
+
+describe('puppeteer', () => {
+  describe(locateChromeExecutable.name, () => {
+    it('should return a path', () => {
+      expect(locateChromeExecutable()).toBeTruthy()
+    })
+  })
+
+  describe(hasExtras.name, () => {
+    it('should return false when no params are given', () => {
+      expect(hasExtras()).toBe(false)
+    })
+
+    it('should return false when one of the plugins is explicitly disabled', () => {
+      expect(hasExtras({ stealth: false })).toBe(false)
+    })
+
+    it('should return true when one of the plugins is enabled', () => {
+      expect(hasExtras({ stealth: true })).toBe(true)
+    })
+  })
+
+  describe(launch.name, () => {
+    it('should launch without extras', async () => {
+      const browser = await launch({ extra: undefined, headless: true })
+
+      expect(browser).toBeTruthy()
+
+      await browser.close()
+    })
+
+    it('should launch with extras', async () => {
+      const browser = await launch({ extra: { stealth: true }, headless: true })
+
+      expect(browser).toBeTruthy()
+
+      await browser.close()
+    })
+  })
+})
--- test/index.test.ts
+++
@@ -1,5 +0,0 @@
-import { helloWorld } from '../src/index'
-
-test('should pass', () => {
-  expect(helloWorld()).toBe('hello world')
-})
